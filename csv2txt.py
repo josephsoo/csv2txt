41df8b5208559325402e812a5e48d4ffa3c7fe15
josephsoo <soojoseph65@gmail.com>
2023-07-04 15:55:20 -0700

    done

---
+++ csv2txt.py
@@ -0,0 +1,40 @@
+import os
+
+# Specify the root directory path
+root_directory = "/Users/josephsoo/Documents/Test"
+
+
+# Recursive function to rename files
+def rename_files(directory):
+    # Get the list of files in the directory
+    files = os.listdir(directory)
+
+    # Iterate over each file in the directory
+    for file in files:
+        # Construct the current file path
+        current_path = os.path.join(directory, file)
+
+        # Check if the current item is a file
+        if os.path.isfile(current_path):
+            # Check if the file ends with ".csv"
+            if file.endswith(".csv"):
+                # Generate the new file name by replacing the extension with ".txt"
+                new_name = file[:-4] + ".txt"
+                
+                # Construct the new file path
+                new_path = os.path.join(directory, new_name)
+                
+                # Rename the file
+                os.rename(current_path, new_path)
+                
+                print(f"Renamed {file} to {new_name}")
+        
+        # Check if the current item is a directory
+        elif os.path.isdir(current_path):
+            # Recursively call the function for subdirectories
+            rename_files(current_path)
+
+# Call the function with the root directory
+rename_files(root_directory)
+
+print("File renaming completed!")
